8ce33e3619e73f070a9ab08c4077f0a1b3485f23
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-13 23:25:12 +0200

    Add ui process

--- js/app.js
+++ js/app.js
@@ -3,21 +3,27 @@ const firstSelect = document.querySelector('#firstCurrency');
 const secondSelect = document.querySelector('#secondCurrency');
 
 const currency = new Currency('USD', 'TRY');
+const ui = new UI(firstSelect, secondSelect);
 
 // Events
 amountElement.addEventListener('input', exchangeCurrency);
+
 firstSelect.onchange = function () {
     currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
+    ui.changeFirst();
+    exchangeCurrency();
 
 }
 
 secondSelect.onchange = function () {
     currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+    ui.changeSecond();
+    exchangeCurrency();
 }
 
 function exchangeCurrency() {
     currency.changeAmount(amountElement.value);
     currency.exchange()
-        .then(result => console.log(result))
+        .then(result => ui.displayResult(result))
         .catch(err => console.log(err));
 }
